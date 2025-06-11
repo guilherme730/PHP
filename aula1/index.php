2284de14187926f007fa68675c45523cb161683c
GUILHERME CARDOSO DE CAMARGO <guilherme.ccamargo5@senacsp.edu.br>
2025-06-11 11:08:55 -0300

    PHP

---
+++ aula1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo</title>
+</head>
+
+<body>
+    <?php
+    # primeiro código 
+    // echo "Olá, Mundo!";
+    // imprime a mensagem Olá Mundo na tela
+    /* comentários em linhas podem ser feitos com os símbolos # e //, já que os comentários com mais de uma linha devem ser feitos com os sinais /* e */
+    // phpinfo();
+
+    // Variáveis 
+
+    $texto = "<h1>Sou aluno da TI36 no Senac de Botucatu</h1>";
+    echo $texto;
+
+    $divisao =
+        "<br>==============<br>";
+    echo $divisao;
+    $nome = "Chaves";
+    $sobrenome = "del Ocho";
+    echo "Olá, meu nome é $nome e meu sobrenome é $sobrenome. ";
+    ?>
+</body>
+
+</html>
